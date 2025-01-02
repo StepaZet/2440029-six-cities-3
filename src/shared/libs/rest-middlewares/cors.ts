19b16997727f6728ca155c003ab3e3e5449e8239
Godov Stepan <stepa.zet@gmail.com>
2025-01-02 14:22:17 +0500

    refactor

--- src/shared/libs/rest-middlewares/cors.ts
+++ src/shared/libs/rest-middlewares/cors.ts
@@ -4,6 +4,8 @@ import { Middleware } from './middleware.interface.js';
 export class CorsMiddleware implements Middleware {
   public async handleAsync(_req: Request, res: Response, next: NextFunction): Promise<void> {
     res.set('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   }
 }
